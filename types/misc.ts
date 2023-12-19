d95fca80f9247ac69c2bd1c83ad4f8d777ee0563
leondaz <ahmed.ibrahim@bld.ai>
2023-12-19 17:50:52 +0200

    fix pycharm autoimports AGAIN

--- types/misc.ts
+++ types/misc.ts
@@ -1,5 +1,4 @@
-import {PostgrestError} from "@supabase/supabase-js";
-import {PostgrestSingleResponse} from "@supabase/postgrest-js/src/types";
+import {PostgrestError, PostgrestSingleResponse} from "@supabase/supabase-js";
 
 export type TDeferrableFunc<T> = (...args: any[]) => Promise<T | void>
 export type TDeferredActionResult<T> = [(...args: Parameters<any>) => void, boolean, T | null, PostgrestError | Error | null]
